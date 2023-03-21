092ad5b3df46aa596c95261a80a01cc1f7eff024
YuliaKorchahina <106467729+YuliaKorchahina@users.noreply.github.com>
2023-03-21 13:07:46 +0000

    Deploying to gh-pages from @ YuliaKorchahina/goit-react-hw-05-movies@889f78d4f93c9ed39769ebede34513646229f976 🚀

---
+++ static/js/805.6dd105e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);var n=r(1087),o=r(184);t.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:"Sorry, page not found"}),(0,o.jsx)("img",{src:"https://cutt.ly/x4imRWt",alt:"Not found hage"}),(0,o.jsx)(n.rU,{to:"/",children:"To Home page"})]})}}}]);
+//# sourceMappingURL=805.6dd105e2.chunk.js.map
